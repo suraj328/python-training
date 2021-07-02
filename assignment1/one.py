f4e4a01613f446810971dec921acd16f28f60386
suraj328 <shahsuraj328@gmail.com>
2021-07-03 00:13:56 +0545

    assignment1

---
+++ assignment1/one.py
@@ -0,0 +1,23 @@
+#Write a Python code to calculate Permutation (5,3)
+#formula=n!/(n-r)!
+n=5
+r=3
+factorial=1
+
+#for neuminator(n!)
+i=1
+numenator=1
+while i<=n:
+    numenator*=i
+    i+=1
+
+
+#for deuminator
+deuminator=1
+j=1
+while j<=(n-r):      #for (n-r)!
+    deuminator*=j
+    j+=1
+
+#reslut of permutation
+print("permutation is",int(numenator/deuminator))
