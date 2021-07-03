e33b45288f4528f6583eef320d4d9d053efba7cd
suraj328 <shahsuraj328@gmail.com>
2021-07-03 17:21:37 +0545

    assignment1.py

---
+++ assignment1/three.py
@@ -0,0 +1,4 @@
+#Write a Python code that takes the degree as input from the user and convert it into radian
+degree=int(input("enter a degree:"))
+radian=degree*(22/7/180)
+print("your radian:",radian)
