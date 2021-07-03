fb6511d1c7aaf0294a7ecc2dd2ae61b7c7eb1090
suraj328 <shahsuraj328@gmail.com>
2021-07-03 19:23:00 +0545

    assignment1

---
+++ assignment1/four.py
@@ -0,0 +1,4 @@
+#Write a Python code that inputs input from the user and calculate its square root
+import math
+numb=int(input("Enter Your Number"))
+print("Squrae Root of youur number is",int(math.sqrt(numb)))
