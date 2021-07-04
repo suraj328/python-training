da796aa5339f6ae90806a685b7ba7462148e6f74
suraj328 <shahsuraj328@gmail.com>
2021-07-04 21:30:04 +0545

    assignment1_complete

---
+++ assignment1/eight.py
@@ -0,0 +1,2 @@
+#Write a program to convert 108 to binary.
+print(f"binary output of {108} is {bin(108)}")
---
+++ assignment1/nine.py
@@ -0,0 +1,2 @@
+#Write a program to convert 1008 to hexadecimal.
+print(f"output of {1008} is {hex(1008)}")
