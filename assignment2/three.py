df4649901dc10ef53b7263f46c675f8c6aa3c396
suraj328 <shahsuraj328@gmail.com>
2021-07-06 20:20:59 +0545

    assignment2_completed

---
+++ assignment2/three.py
@@ -0,0 +1,5 @@
+#program to demonstrate data types that can be elements of a tuple.
+#A Program to demonestrate data types of tupple element by:type()
+store=("Age",21)
+print(store)
+print(list(map(type,store)))
